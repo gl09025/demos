bb75bf0c523d784f579b55236d610955bbd08d0e
ganli <287893726@qq.com>
2017-04-07 11:01:37 +0800

    添加阶乘的断言,判断是否为NaN

--- 18lesson/facrotial.js
+++ 18lesson/facrotial.js
@@ -1,8 +1,12 @@
 
 
 function factorial(n){
-
-        return (n === 0) ? 1:(n * factorial(n-1))
+  if(typeof n === 'string'){
+          return NaN
+  }
+  return (n === 0) ? 1:(n * factorial(n-1))
 }
 
-console.log (factorial(3))
+console.assert(factorial(1) === 1)
+console.assert(factorial(6) === 6*5*4*3*2*1)
+console.assert(Number.isNaN(factorial('string')) === true)  
