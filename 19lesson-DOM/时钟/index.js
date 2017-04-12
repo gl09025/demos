5dee434a69eeab21b5cf11c5e62cb37ec4ffd05c
ganli <287893726@qq.com>
2017-04-12 16:18:35 +0800

    时钟,利用JS来生成三个指针div

--- 19lesson-DOM/时钟/index.js
+++ 19lesson-DOM/时钟/index.js
@@ -1,3 +1,35 @@
+function makehands(){
+//使用JS创建三个针
+    var sh = document.createElement('div')
+    var mh = document.createElement('div')
+    var hh = document.createElement('div')
+
+    sh.className = 'second-hand'    
+    mh.className = 'minute-hand'
+    hh.className = 'hour-hand'
+
+    var clock = document.querySelector('.clock')
+    clock.appendChild(sh)
+    clock.appendChild(mh)
+    clock.appendChild(hh)
+//计算角度
+    var now = new Date()
+
+    var hours = now.getHours()
+    var minutes = now.getMinutes()
+    var seconds = now.getSeconds()
+
+    if(hours > 12){
+        hours -= 12
+    }
+    var hoursDeg = hours * 30 + minutes * 0.5 
+    var minutesDeg = minutes * 6 + (seconds * 0.1)
+    var secondsDeg = seconds * 6
+    hh.style.transform = `rotate(${hoursDeg}deg)`
+    mh.style.transform = `rotate(${minutesDeg}deg)`
+    sh.style.transform = `rotate(${secondsDeg}deg)`
+}
+
 function step(){
     var now = new Date()
 
@@ -29,7 +61,7 @@ function step(){
     document.querySelector('.hour-hand').style.transform = `rotate(${hoursDeg}deg)`
     document.querySelector('.minute-hand').style.transform = `rotate(${minutesDeg}deg)`
     document.querySelector('.second-hand').style.transform = `rotate(${secondsDeg}deg)`
-    document.getElementById('clock').removeAttribute("hidden")
+    // document.getElementById('clock').removeAttribute("hidden")
 }
   //添加罗马数字
   var roman = function(){
@@ -46,7 +78,8 @@ function step(){
             oli.style.listStyleType='none'
         }
   }
-    
+
+makehands()  
 roman()
 setInterval(function(){
     step()
