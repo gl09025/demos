aec209583e288023106d3d957f6c89af8d84bb22
ganli <287893726@qq.com>
2017-05-11 00:04:38 +0800

    简单页面初步实现

---
+++ 29lesson/initializr/js/main.js
@@ -0,0 +1,26 @@
+
+var swiper = new Swiper('#firstswiper', {
+    pagination: '.swiper-pagination',
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflow: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows : true
+    }
+});
+
+    var swiper2 = new Swiper('.daily-down>.swiper-container', {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        paginationClickable: true,
+        spaceBetween: 0,
+        centeredSlides: true,
+        autoplay: 3500,
+        autoplayDisableOnInteraction: false
+    });
