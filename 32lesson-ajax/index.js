40c87faffd1d98b0c831f6e81948b62795a1f70d
ganli <287893726@qq.com>
2017-05-14 16:29:14 +0800

    ajax,学习ajax的简单语法.在本地跑一个静态的node服务，在index.html里添加按钮点击事件，里面用ajax来请求json数据，处理json数据插入页面中

---
+++ 32lesson-ajax/index.js
@@ -0,0 +1,42 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+
+//console.log(Object.keys(http))
+var port = process.env.PORT || 8888;
+
+var server = http.createServer(function(request, response){
+
+  var temp = url.parse(request.url, true)
+  var path = temp.pathname
+  var query = temp.query
+
+  //从这里开始看，上面不要看
+
+  if(path === '/'){
+    var string = fs.readFileSync('./index.html') 
+    response.end(string)
+  }if(path === '/style.css'){
+    var string = fs.readFileSync('./style.css');
+    response.end(string)
+  }if( path === '/main.js'){
+    var string = fs.readFileSync('./main.js')
+    response.setHeader('Content-Type','application/javascript')
+    response.end(string)
+  }else if( path === '/page1.json'){
+    var string = fs.readFileSync('./page1.json')
+    response.setHeader('Content-Type','application/xml')
+    response.end(string)
+  }else if( path === '/page2.json'){
+    var string = fs.readFileSync('./page2.json')
+    response.setHeader('Content-Type','application/xml')
+    response.end(string)
+  }else{
+    response.end('404')
+  }
+
+  // 代码结束，下面不要看
+})
+
+server.listen(port)
+console.log('监听' + port + '成功')
