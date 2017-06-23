141cd5486d52126984c93f753f0790f7a186e3ec
gl09025 <287893726@qq.com>
2017-06-23 10:33:48 +0800

    用js添加歌词

--- NetEaseMusic/song.js
+++ NetEaseMusic/song.js
@@ -2,7 +2,28 @@
  * Created by Andrew on 2017/6/22.
  */
 $(function(){
+    $.get('./lyric.json').then(function(object){
+        let {lyric} = object
+        let array = lyric.split('\n')
+        let regex = /^\[(.+)\](.*)$/
+        array = array.map(function(string, index){
+            let matches = string.match(regex)
+            if(matches){
+                return {time: matches[1], words: matches[2]}
+            }
+        })
+        console.log(array)
+        let $lyric = $('.m-song-scroll')
+        console.log($lyric)
+        array.map(function(object){
+            if(!object){return}
+            let $p = $('<p/>')
+            $p.attr('data-time', object.time).text(object.words)
+            $p.appendTo($lyric.children('.m-song-iner'))
+        })
+    })
 
+    //播放歌曲
     let audio = document.createElement('audio')
     audio.src = './Alan Walker - Fade.mp3'
     audio.oncanplay = function(){
