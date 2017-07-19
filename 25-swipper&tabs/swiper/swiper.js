95dddabb8f4f05f4ab71be5e53f99067fd615e23
gl09025 <gl09025@users.noreply.github.com>
2017-07-19 15:51:46 +0800

    Update swiper.js

--- 25-swipper&tabs/swiper/swiper.js
+++ 25-swipper&tabs/swiper/swiper.js
@@ -96,7 +96,7 @@ window.slides = function(element) {
         timerId = setInterval(function() {
             goToSlide(currentIndex + 1)
             becomeActive(currentIndex)
-        }, 3000)
+        }, 300)
     }
     autoPlay()
 
